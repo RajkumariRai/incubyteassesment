860951ccf720b4cf17d95c1d797ef9e2eb7b1b95
RajkumariRai <pujarai2013@gmail.com>
2024-07-21 21:43:25 +0530

    changes for failed test case

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+// src/App.tsx
 import React from 'react';
-import './App.css';
 import StringCalculator from './components/StringCalculator';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <h1>String Calculator</h1>
       <StringCalculator />
     </div>
   );
 };
 
 export default App;
-
